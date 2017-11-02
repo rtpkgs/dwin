083183983e10578b3fe4d483e4dc39aea198da76
liu2guang <1004383796@qq.com>
2017-11-02 10:37:09 +0800

    [创建] 创建dwlib库中需要移植的接口, 默认PORT接口中是使用的RT-Thread串口框架和sem

---
+++ dwlib/port/dw_port.h
@@ -0,0 +1,18 @@
+#ifndef __DW_PORT_H_
+#define __DW_PORT_H_
+
+/* dwlib common header file */
+#include "dw_common.h" 
+
+#define DW_RESOURCE_TAKE(timeout) dw_port_serial_resource_take(timeout)
+#define DW_RESOURCE_RELEASE()     dw_port_serial_resource_release()
+
+/* Port API */
+bool dw_port_serial_init(const char *name, uint32_t baudrate);
+bool dw_port_serial_putc(char  byte);
+bool dw_port_serial_getc(char *byte);
+bool dw_port_serial_resource_init(void);
+bool dw_port_serial_resource_take(int32_t timeout);
+bool dw_port_serial_resource_release(void);
+
+#endif
