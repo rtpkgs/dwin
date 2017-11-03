99f8eccfd918cdaecc04ad1bbeff3763b8ae85a8
liu2guang <1004383796@qq.com>
2017-11-03 10:22:29 +0800

    [修改] 添加底层api的统一包含头文件

--- dwlib/dw.h
+++ dwlib/dw.h
@@ -1,6 +1,18 @@
 #ifndef __DW_H_
 #define __DW_H_
 
+/* dwlib common header file */
 #include "dw_common.h"
+
+/* dwlib all api */
+
+/* dwlib basic function api */
+#include "dw_init.h"
+#include "dw_system.h"
+
+/* dwlib plugins function api */
+#if defined(PKG_DWLIB_ENABLE_SM)
+#include "dw_space_manager.h"
+#endif
 
 #endif
