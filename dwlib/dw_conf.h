f555522d737f38380ba991540692e35e736833b3
liu2guang <1004383796@qq.com>
2017-11-03 23:44:42 +0800

    [修改] 优化rtt3.0 env开发坏境, 适配menuconfig

--- dwlib/dw_conf.h
+++ dwlib/dw_conf.h
@@ -2,15 +2,39 @@
 #define __SW_CONF_H_ 
 
 #include "dw_port.h"
-#include "rtthread.h"
 
 /* 内存分配 */
 #if !defined(DW_MALLOC) 
+#if defined(PKG_DWLIB_MM_RTT)
+#define DW_MALLOC rt_malloc
+#endif
+#if defined(PKG_DWLIB_MM_C)
 #define DW_MALLOC malloc
+#endif
 #endif
 
 #if !defined(DW_FREE) 
+#if defined(PKG_DWLIB_MM_RTT)
+#define DW_FREE rt_free
+#endif
+#if defined(PKG_DWLIB_MM_C)
 #define DW_FREE free
+#endif
+#endif
+
+/* 打印接口 */
+#if !defined(dw_print) 
+#include "rtthread.h"
+#if defined(PKG_DWLIB_PRINT_RTT)
+#define dw_print(...) rt_kprintf("[dwlib] "); rt_kprintf(__VA_ARGS__); rt_kprintf("\n")
+#endif
+#if defined(PKG_DWLIB_PRINT_ELOG)
+#include "elog.h"
+#define dw_print(...) elog_i("dwlib", __VA_ARGS__)
+#endif
+#if defined(PKG_DWLIB_PRINT_C)
+#define dw_print(...) printf("[dwlib] "); printf(__VA_ARGS__); printf("\n")
+#endif
 #endif
 
 /* dwlib库空间管理插件 */
