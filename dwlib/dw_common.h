01296cbda836b8e6ad8dd48cc14ed49b2e8fa54a
liu2guang <1004383796@qq.com>
2017-11-03 10:22:59 +0800

    [添加] 添加对clist库的检测

--- dwlib/dw_common.h
+++ dwlib/dw_common.h
@@ -7,4 +7,11 @@
 /* suppurt uintx_t type */
 #include <stdint.h>
 
+/* suppurt clib list */
+#include "list.h"
+#if !defined(LIST_VERSION)
+    #error dwlib need clib list suppurt. 
+    #error plases download from 'https://github.com/liu2guang/clist/archive/v1.0.0.zip'
+#endif
+
 #endif
