73d4c1a8ad4a23a5c8944c74af6e92e01c263ba1
liu2guang <1004383796@qq.com>
2017-11-03 10:23:50 +0800

    [添加] 添加空间分配策略相关函数接口

--- dwlib/plugins/spacemanager/dw_space_manager.c
+++ dwlib/plugins/spacemanager/dw_space_manager.c
@@ -1,28 +1,56 @@
 #include "dw_space_manager.h"
-#include <stdbool.h>
-#include <stdint.h>
+#include "string.h"
 
-#if 0
-typedef struct
-{
-    const char name[xxx];
-    
-    uint16_t addr;
-    uint32_t size;
-} dw_space_block;
-#endif
+/* 空闲空间首地址 */
+static uint16_t sm_idle_addr = 0x0000;
 
 /* 初始化变量空间管理器 */
-bool dw_plugin_sm_init(int32_t space_byte)
+bool dw_plugin_sm_init(void)
 {
+    /* 初始化空闲空间 */
+    sm_idle_addr = 0x0000;
+    
     return true;
 }
 
-/* 申请n字节的变量空间 */
-bool dw_plugin_sm_apply(uint32_t size)
+/* 申请n字节的变量空间, 并命名 */
+dw_space_block_t dw_plugin_sm_apply(const char *name, uint32_t size)
 {
-    return true;
+    dw_space_block_t space = NULL;
+    
+    /* 判断参数 */
+    if(name == NULL || size == 0)
+    {
+        return NULL;
+    }
+    
+    /* 计算空闲内存大小 */
+    if(dw_plugin_sm_idle_space() < size)
+    {
+        return NULL;
+    }
+    
+    /* 分配内存 */
+    space = DW_MALLOC(sizeof(struct dw_space_block));
+    
+    /* 拷贝名称 */
+    if(strcpy(space->name, name) == NULL)
+    {
+        return NULL;
+    }
+    
+    /* 获取申请的空间大小与地址 */
+    space->size = size;
+    space->addr = sm_idle_addr;
+    
+    /* 计算新的空间空闲地址 */
+    sm_idle_addr += size;
+    
+    return space;
 }
 
-/* 释放n字节的变量空间 */
-// 暂时没有这个需求
+/* 获取空间剩余大小 */
+uint16_t dw_plugin_sm_idle_space(void)
+{
+    return (PKG_DWLIB_SM_SPACE_MAXSIZE - sm_idle_addr);
+}
---
+++ dwlib/plugins/spacemanager/dw_space_manager.h
@@ -0,0 +1,19 @@
+#ifndef __SW_SPACE_MANGER_H_ 
+#define __SW_SPACE_MANGER_H_ 
+
+#include "dw_common.h"
+#include "dw_conf.h"
+
+typedef struct dw_space_block
+{
+    uint16_t addr;
+    uint32_t size;
+    
+    char name[PKG_DWLIB_SM_NAME_LENGTH];
+} *dw_space_block_t;
+
+bool dw_plugin_sm_init(void);
+dw_space_block_t dw_plugin_sm_apply(const char *name, uint32_t size);
+uint16_t dw_plugin_sm_idle_space(void);
+
+#endif 
