4109ea6ab687391e068801121917433689a6b56a
liu2guang <1004383796@qq.com>
2017-11-03 23:43:56 +0800

    [添加] 添加链表功能, 每个申请的空间结构体都会被链表进行管理, 同时添加空间使用信息打印api

--- dwlib/plugins/spacemanager/dw_space_manager.c
+++ dwlib/plugins/spacemanager/dw_space_manager.c
@@ -4,11 +4,20 @@
 /* 空闲空间首地址 */
 static uint16_t sm_idle_addr = 0x0000;
 
+/* 已经分配的空间管理链表头 */
+static list_t *sm_used_head_list = NULL;
+
 /* 初始化变量空间管理器 */
 bool dw_plugin_sm_init(void)
 {
     /* 初始化空闲空间 */
     sm_idle_addr = 0x0000;
+    
+    sm_used_head_list = list_new();
+    if(sm_used_head_list == NULL)
+    {
+        return false;
+    }
     
     return true;
 }
@@ -16,6 +25,7 @@ bool dw_plugin_sm_init(void)
 /* 申请n字节的变量空间, 并命名 */
 dw_space_block_t dw_plugin_sm_apply(const char *name, uint32_t size)
 {
+    list_node_t *node = NULL;
     dw_space_block_t space = NULL;
     
     /* 判断参数 */
@@ -43,6 +53,10 @@ dw_space_block_t dw_plugin_sm_apply(const char *name, uint32_t size)
     space->size = size;
     space->addr = sm_idle_addr;
     
+    /* 添加内存结构体指针插入到链表中 */
+    node = list_node_new((void *)space);
+    list_rpush(sm_used_head_list, node);
+    
     /* 计算新的空间空闲地址 */
     sm_idle_addr += size;
     
@@ -54,3 +68,36 @@ uint16_t dw_plugin_sm_idle_space(void)
 {
     return (PKG_DWLIB_SM_SPACE_MAXSIZE - sm_idle_addr);
 }
+
+/* 打印已经使用的内存 */
+void dw_plugin_sm_used_space(void)
+{
+    list_node_t *node = NULL;
+    list_iterator_t *iterator = NULL;
+    
+    if(sm_used_head_list == NULL)
+    {
+        dw_print("no used space");
+        return;
+    }
+    
+    iterator = list_iterator_new(sm_used_head_list, LIST_HEAD);
+    
+    while((node = list_iterator_next(iterator)) != NULL)
+    {
+        dw_print("<space name> = %s <addr> = 0x%.4x <size> = %d", 
+            ((struct dw_space_block *)(node->val))->name, 
+            ((struct dw_space_block *)(node->val))->addr, 
+            ((struct dw_space_block *)(node->val))->size);
+    }
+    
+    dw_print("current space addr = 0x%.4x, used space size = %dByte, free space size = %dByte", 
+        sm_idle_addr, sm_idle_addr, dw_plugin_sm_idle_space());
+    
+    list_iterator_destroy(iterator);
+}
+
+/* 下面的带准备搬移到finsh命令插件中, 空间管理插件不需要clist以外的外部库依赖 */
+#include "finsh.h"
+MSH_CMD_EXPORT_ALIAS(dw_plugin_sm_used_space, sm, print used space);
+FINSH_FUNCTION_EXPORT_ALIAS(dw_plugin_sm_used_space, sm, print used space);
--- dwlib/plugins/spacemanager/dw_space_manager.h
+++ dwlib/plugins/spacemanager/dw_space_manager.h
@@ -1,7 +1,6 @@
 #ifndef __SW_SPACE_MANGER_H_ 
 #define __SW_SPACE_MANGER_H_ 
 
-#include "dw_common.h"
 #include "dw_conf.h"
 
 typedef struct dw_space_block
@@ -15,5 +14,6 @@ typedef struct dw_space_block
 bool dw_plugin_sm_init(void);
 dw_space_block_t dw_plugin_sm_apply(const char *name, uint32_t size);
 uint16_t dw_plugin_sm_idle_space(void);
+void dw_plugin_sm_used_space(void);
 
 #endif 
