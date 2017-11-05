fb8cf97ae0ebf296750446a2f535dbc78c761ab3
liu2guang <1004383796@qq.com>
2017-11-06 03:13:25 +0800

    [添加] 添加位操作宏

--- dwlib/dw_common.h
+++ dwlib/dw_common.h
@@ -14,4 +14,25 @@
     #error plases download from 'https://github.com/liu2guang/clist/archive/v1.0.0.zip'
 #endif
 
+#define DW_VERSION      1L
+#define DW_SUBVERSION   1L
+#define DW_REVISION     0L
+
+/* Ö¸Áî */
+#define DW_REG_W_CMD    (0x80)
+#define DW_REG_R_CMD    (0x81)
+#define DW_SPACE_W_CMD  (0x82)
+#define DW_SPACE_R_CMD  (0x83)
+
+/* Î»²Ù×÷ */
+#define DW_BITx(n) (1 << ((n)-1))
+#define DW_BITS_8  (0xFF)
+#define DW_BITS_16 (0xFFFF)
+#define DW_BITS_32 (0xFFFFFFFF) 
+
+#define DW_BITS_SET(data, bits)  ((data) |=  (bits)) 
+#define DW_BITS_CLR(data, bits)  ((data) &= ~(bits)) 
+#define DW_BITS_TGL(data, bits)  ((data) ^=  (bits)) 
+#define DW_BITS_READ(data, bits) ((data) &   (bits))
+
 #endif
