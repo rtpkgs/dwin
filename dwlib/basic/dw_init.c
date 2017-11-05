9e61af312a3154f39845ae49ad0fa52b7205fd49
liu2guang <1004383796@qq.com>
2017-11-06 03:12:37 +0800

    [添加] 添加dwlib库初始化函数, 调用该函数初始化所有功能

--- dwlib/basic/dw_init.c
+++ dwlib/basic/dw_init.c
@@ -1,2 +1,32 @@
-#include "dw.h"
+#include "dw_init.h"
+#include "dw_port.h"
 
+#if defined(PKG_DWLIB_ENABLE_SM)
+#include "dw_space_manager.h"
+#endif
+
+/* dwlib≥ı ºªØ */
+bool dw_init(const char *name, uint32_t baudrate)
+{
+    if(dw_port_serial_resource_init() == false)
+    {
+        dw_print("serial resource init failed");
+        return false;
+    }
+    
+    if(dw_port_serial_init(name, baudrate) == false)
+    {
+        dw_print("serial init failed");
+        return false;
+    }
+    
+#if defined(PKG_DWLIB_ENABLE_SM)
+    if(dw_plugin_sm_init() == false)
+    {
+        dw_print("space manager init failed");
+        return false;
+    }
+#endif
+    
+    return true;
+}
---
+++ dwlib/basic/dw_init.h
@@ -0,0 +1,10 @@
+#ifndef __DW_INIT_H_ 
+#define __DW_INIT_H_ 
+
+#include "dw_common.h"
+#include "dw_conf.h"
+
+/* init api */
+bool dw_init(const char *name, uint32_t baudrate);
+
+#endif
