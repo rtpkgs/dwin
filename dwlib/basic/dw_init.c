f18b7a256c87484c2380adc5c0ea76f562f473ab
liu2guang <1004383796@qq.com>
2017-11-27 03:14:54 +0800

    [添加] 添加button初始化功能, 添加后台轮询函数, 但是下一版本轮询函数会被独立出来

--- dwlib/basic/dw_init.c
+++ dwlib/basic/dw_init.c
@@ -6,9 +6,32 @@
 #include "dw_space_manager.h"
 #endif
 
+#if defined(PKG_DWLIB_ENABLE_BUTTON)
+#include "dw_button.h"
+#endif
+
+#if defined(PKG_DWLIB_ENABLE_BUTTON)
+void dwin_event_thread_entry(void *p)
+{
+    for(;;)
+    {
+        /* 按键事件处理回调 */
+#if defined(PKG_DWLIB_ENABLE_BUTTON)
+        dw_plugin_button_event_poll();
+#endif
+        
+        rt_thread_delay(RT_TICK_PER_SECOND/10);
+    }
+}
+#endif
+
 /* dwlib初始化 */
 bool dw_init(const char *name, uint32_t baudrate)
 {
+#if defined(PKG_DWLIB_ENABLE_BUTTON)
+    rt_thread_t tdwin_event = NULL;
+#endif
+    
     if(dw_port_serial_resource_init() == false)
     {
         dw_print("serial resource init failed");
@@ -22,7 +45,7 @@ bool dw_init(const char *name, uint32_t baudrate)
     }
     
     /* 关闭自动上传功能 */
-    dw_touch_data_update_enable(false);
+    dw_touch_data_update_enable(false); /* 不起作用? */
     
     /* 初始化L22字库加载空间变量功能 */
     dw_l22_mode_init();
@@ -47,6 +70,30 @@ bool dw_init(const char *name, uint32_t baudrate)
         dw_print("space manager init failed");
         return false;
     }
+#endif
+    
+#if defined(PKG_DWLIB_ENABLE_BUTTON)
+    if(dw_plugin_button_init() == false)
+    {
+        dw_print("button init failed");
+        return false;
+    }
+#endif
+    
+    
+    /* 事件处理函数 */
+#if defined(PKG_DWLIB_ENABLE_BUTTON)
+    tdwin_event = rt_thread_create(
+        "tdwin_event", dwin_event_thread_entry, RT_NULL, 
+        512, 5, 10);
+    
+    if(tdwin_event == RT_NULL)
+    {
+        dw_print("event thread create failed");
+        return false;
+    }
+    
+    rt_thread_startup(tdwin_event);
 #endif
     
     return true;
