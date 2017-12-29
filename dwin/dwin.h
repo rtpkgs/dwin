e7f2933def63d7e6ac95cefd04a95a13e25b0dca
liu2guang <1004383796@qq.com>
2017-12-29 15:29:27 +0800

    [修复] 修复dwin支持插件裁剪小bug

--- dwin/dwin.h
+++ dwin/dwin.h
@@ -2,9 +2,21 @@
 #define __DWIN_H_
 
 #include "dwin_init.h" 
+
+#ifdef PKG_DWIN_ENABLE_PLUGIN_BUTTON
 #include "dwin_plugin_button.h" 
+#endif
+
+#ifdef PKG_DWIN_ENABLE_PLUGIN_INPUTBOX
 #include "dwin_plugin_inputbox.h" 
+#endif
+
+#ifdef PKG_DWIN_ENABLE_PLUGIN_ICON
 #include "dwin_plugin_icon.h" 
+#endif
+
+#ifdef PKG_DWIN_ENABLE_PLUGIN_TEXTBOX
 #include "dwin_plugin_textbox.h" 
+#endif
 
 #endif 
