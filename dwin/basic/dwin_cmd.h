8b60d1a75bd4a79968f01ea3a2bb5ebe5f425a75
liu2guang <1004383796@qq.com>
2018-04-24 17:18:22 +0800

    [update] add read and write reg/var cmd. | 添加读写寄存器msh命令.

---
+++ dwin/basic/dwin_cmd.h
@@ -0,0 +1,19 @@
+/*
+ * @File:   dwin_cmd.c 
+ * @Author: liu2guang 
+ * @Date:   2018-04-22 14:52:10 
+ * 
+ * @LICENSE: MIT
+ * https://github.com/liu2guang/dwin/blob/master/LICENSE
+ * 
+ * Change Logs: 
+ * Date           Author       Notes 
+ * 2018-04-24     liu2guang    update v2 framework. 
+ */ 
+
+#ifndef __DWIN_CMD_H__ 
+#define __DWIN_CMD_H__ 
+
+#include "dwin_def.h" 
+
+#endif
